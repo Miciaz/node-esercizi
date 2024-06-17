cd02369a7716bf95b5da777bdd93b4ef90ba27fe
Rossella Sonzogni <ross.sonzogni@gmail.com>
2024-06-17 14:02:27 +0200

    added event-listener

---
+++ event-listener.mjs
@@ -0,0 +1,40 @@
+import { EventEmitter } from "node:events"
+
+function createNewsFeed() {
+  const emitter = new EventEmitter();
+
+  setInterval(() => {
+    emitter.emit("newsEvent", "News: A thing happened in a place.");
+  }, 1000);
+
+  setInterval(() => {
+    emitter.emit("breakingNews", "Breaking news! A BIG thing happened.");
+  }, 4000);
+
+  setTimeout(() => {
+    emitter.emit("error", new Error("News feed connection error"));
+  }, 5000);
+
+  return emitter;
+}
+
+const newsFeed = createNewsFeed();
+
+newsFeed.on("newsEvent", (data) => {
+  console.log(`Received news event: ${data}`);
+});
+
+newsFeed.on("breakingNews", (data) => {
+  console.log(`Received breaking news event: ${data}`);
+});
+
+newsFeed.on("error", (error) => {
+  console.error(`Error occurred: ${error.message}`);
+});
+
+
+newsFeed.emit("newsEvent");
+newsFeed.emit("breakingNews");
+setTimeout(() => {
+  newsFeed.emit("error");
+}, 6000);
